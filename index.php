28620a0bc6c731d54f68ba12901e347b3e753430
Maxim-front <mkorzhenevskiy2000@mail.ru>
2023-12-13 01:05:00 +0300

    task 3 (преимущества макет) and images

--- index.php
+++ index.php
@@ -76,6 +76,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['phone_number'])) {
     <title>Your Page</title>
     <!-- Include Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <!-- Include Swiper CSS -->
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css"/>
+
     <style>
         * {
             padding: 0;
@@ -110,8 +113,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['phone_number'])) {
         #cookie-popup {
             display: none;
             position: fixed;
-            bottom: 0px;
-            right: 0px;
+            bottom: 0;
+            right: 0;
             max-width: 80%;
             padding: 15px;
             background-color: #f8d7da;
@@ -125,6 +128,115 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['phone_number'])) {
             margin: 0 auto;
             padding: 0 0.75rem;
             box-sizing: content-box;
+        }
+
+        .benefits-title {
+            font-size: 40px;
+            font-weight: 700;
+            line-height: 130%;
+            margin-bottom: 48px;
+        }
+
+        .cards-wrapper {
+            display: flex;
+            flex-wrap: wrap;
+            gap: 20px;
+            justify-content: space-between;
+
+        }
+
+        .my-card {
+            width: 295px;
+            height: 289px;
+            flex-shrink: 0;
+            border-radius: 4px;
+            background: #F9F9F9;
+            padding: 32px 24px;
+        }
+
+        .my-card-title {
+            margin-bottom: 16px;
+            font-weight: 700;
+            font-size: 20px;
+            line-height: 150%; /* 30px */
+        }
+
+        .my-card-text {
+            font-size: 16px;
+            font-weight: 400;
+            line-height: 130%; /* 20.8px */
+        }
+
+        .swiper-container {
+            width: 100%;
+            overflow-y: visible;
+            overflow-x: clip;
+        }
+
+        .swiper-slide {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .swiper-pagination-bullet {
+            background-color: #78599C;
+        }
+
+        .swiper-container .my-card {
+            height: 202px;
+        }
+
+        .my-card-img {
+            width: 60px;
+            height: 60px;
+            margin-bottom: 32px;
+        }
+
+        .my-card img {
+            display: block;
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+        }
+
+        .mobile-wrapper {
+            display: none;
+            position: relative;
+        }
+
+        .swiper-pagination {
+            bottom: -24px !important;
+        }
+
+        @media screen and (max-width: 991px) {
+            .desktop-wrapper {
+                display: none;
+            }
+
+            .my-card-img {
+                width: 40px;
+                height: 40px;
+                margin-bottom: 24px;
+            }
+
+            .my-card-text {
+                font-size: 14px;
+            }
+
+            .my-card-title {
+                margin-bottom: 8px;
+                font-size: 16px;
+            }
+
+            .mobile-wrapper {
+                display: block;
+            }
+
+            .benefits-title {
+                font-size: 24px;
+                margin-bottom: 24px;
+            }
         }
     </style>
 </head>
@@ -155,15 +267,175 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['phone_number'])) {
     <button id="close-popup" class="btn btn-danger">Close</button>
 </div>
 
+<div class="my-container">
+    <p class="task-comment">////////////////////// Task 3</p>
+    <h2 class="benefits-title">Korzyści ze współpracy z nami</h2>
+    <div class="cards-wrapper desktop-wrapper">
+        <div class="my-card col-lg-3">
+            <div class="my-card-img">
+                <img src="img/oversized-goods-storage.svg" alt="">
+            </div>
+            <h5 class="my-card-title">Przechowywanie towarów ponadgabarytowych</h5>
+            <p class="my-card-text">Opis</p>
+        </div>
+        <div class="my-card col-lg-3">
+            <div class="my-card-img">
+                <img src="./img/flexible-cooperation-conditions.svg" alt="">
+            </div>
+            <h5 class="my-card-title">Elastyczne warunki współpracy</h5>
+            <p class="my-card-text">Opis</p>
+        </div>
+        <div class="my-card col-lg-3">
+            <div class="my-card-img">
+                <img src="./img/order-integration-management.svg" alt="">
+            </div>
+            <h5 class="my-card-title">Integracja i zarządzanie zamówieniami</h5>
+            <p class="my-card-text">Opis</p>
+        </div>
+        <div class="my-card col-lg-3">
+            <div class="my-card-img">
+                <img src="./img/order-shipping-day.svg" alt="">
+            </div>
+            <h5 class="my-card-title">Wysyłka zamówień
+                w dniu kompletacji</h5>
+            <p class="my-card-text">Opis</p>
+        </div>
+        <div class="my-card col-lg-3">
+            <div class="my-card-img">
+                <img src="./img/low-shipping-costs.svg" alt="">
+            </div>
+            <h5 class="my-card-title">Niskie koszty dostawy</h5>
+            <p class="my-card-text">Opis</p>
+        </div>
+        <div class="my-card col-lg-3">
+            <div class="my-card-img">
+                <img src="./img/goods-security-guarantee.svg" alt="">
+            </div>
+            <h5 class="my-card-title">Gwarancja bezpieczeństwa towarów</h5>
+            <p class="my-card-text">Opis</p>
+        </div>
+        <div class="my-card col-lg-3">
+            <div class="my-card-img">
+                <img src="./img/international-order-shipping.svg" alt="">
+            </div>
+            <h5 class="my-card-title">Całodobowy wideo monitoring</h5>
+            <p class="my-card-text">Opis</p>
+        </div>
+        <div class="my-card col-lg-3">
+            <div class="my-card-img">
+                <img src="./img/international-order-shipping.svg" alt="">
+            </div>
+            <h5 class="my-card-title">Wysyłka zamówień
+                do różnych krajów</h5>
+            <p class="my-card-text">Opis</p>
+        </div>
+    </div>
+    <div class="swiper-container mobile-wrapper" data-pagination="true" data-pagination-dynamic-bullets="true">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide">
+                <div class="my-card">
+                    <div class="my-card-img">
+                        <img src="img/oversized-goods-storage.svg" alt="">
+                    </div>
+                    <h5 class="my-card-title">Przechowywanie towarów ponadgabarytowych</h5>
+                    <p class="my-card-text">Opis</p>
+                </div>
+            </div>
+            <div class="swiper-slide">
+                <div class="my-card">
+                    <div class="my-card-img">
+                        <img src="./img/flexible-cooperation-conditions.svg" alt="">
+                    </div>
+                    <h5 class="my-card-title">Elastyczne warunki współpracy</h5>
+                    <p class="my-card-text">Opis</p>
+                </div>
+            </div>
+            <div class="swiper-slide">
+                <div class="my-card">
+                    <div class="my-card-img">
+                        <img src="./img/order-integration-management.svg" alt="">
+                    </div>
+                    <h5 class="my-card-title">Integracja i zarządzanie zamówieniami</h5>
+                    <p class="my-card-text">Opis</p>
+                </div>
+            </div>
+            <div class="swiper-slide">
+                <div class="my-card">
+                    <div class="my-card-img">
+                        <img src="./img/order-shipping-day.svg" alt="">
+                    </div>
+                    <h5 class="my-card-title">Wysyłka zamówień
+                        w dniu kompletacji</h5>
+                    <p class="my-card-text">Opis</p>
+                </div>
+            </div>
+            <div class="swiper-slide">
+                <div class="my-card">
+                    <div class="my-card-img">
+                        <img src="./img/low-shipping-costs.svg" alt="">
+                    </div>
+                    <h5 class="my-card-title">Niskie koszty dostawy</h5>
+                    <p class="my-card-text">Opis</p>
+                </div>
+            </div>
+            <div class="swiper-slide">
+                <div class="my-card">
+                    <div class="my-card-img">
+                        <img src="./img/goods-security-guarantee.svg" alt="">
+                    </div>
+                    <h5 class="my-card-title">Gwarancja bezpieczeństwa towarów</h5>
+                    <p class="my-card-text">Opis</p>
+                </div>
+            </div>
+            <div class="swiper-slide">
+                <div class="my-card">
+                    <div class="my-card-img">
+                        <img src="./img/video-monitoring.svg" alt="">
+                    </div>
+                    <h5 class="my-card-title">Całodobowy wideo monitoring</h5>
+                    <p class="my-card-text">Opis</p>
+                </div>
+            </div>
+            <div class="swiper-slide">
+                <div class="my-card">
+                    <div class="my-card-img">
+                        <img src="./img/international-order-shipping.svg" alt="">
+                    </div>
+                    <h5 class="my-card-title">Wysyłka zamówień
+                        do różnych krajów</h5>
+                    <p class="my-card-text">Opis</p>
+                </div>
+            </div>
+        </div>
+        <div class="swiper-pagination">
+            <span class="swiper-pagination-bullet"></span>
+            <span class="swiper-pagination-bullet"></span>
+            <span class="swiper-pagination-bullet"></span>
+            <!-- Add more bullets as needed -->
+        </div>
+    </div>
+</div>
+
 <!-- Include Bootstrap JS and jQuery -->
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
         crossorigin="anonymous">
 </script>
-
+<!-- Include Swiper JS -->
+<script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
 <script type="module">
     $(document).ready(() => {
+        // Initialize Swiper
+        const swiper = new Swiper('.swiper-container', {
+            slidesPerView: 1,
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+                dynamicBullets: true,
+            },
+        });
+
         // Function to set a cookie
         function setCookie(name, value, days) {
             let expires = "";
